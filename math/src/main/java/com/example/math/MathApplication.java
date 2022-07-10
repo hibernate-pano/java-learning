a3a398a97d55090c09c5740dce565ce7b1d15efc
hibernate-pano <362472407@qq.com>
2022-07-10 21:57:21 +0800

    Design Patterns add

---
+++ math/src/main/java/com/example/math/MathApplication.java
@@ -0,0 +1,16 @@
+package com.example.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pano
+ */
+@SpringBootApplication
+public class MathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathApplication.class, args);
+    }
+
+}
