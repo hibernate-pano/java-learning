152d15ae14ca5d9544dac2c521f61d322d5569f1
hibernate-pano <362472407@qq.com>
2022-10-13 20:31:17 +0800

    add leetcode 769 321

---
+++ leetcode/src/main/java/com/example/leetcode/leetcode/LeetCode321.java
@@ -0,0 +1,9 @@
+package com.example.leetcode.leetcode;
+
+/**
+ *
+ */
+public class LeetCode321 {
+
+    
+}
---
+++ leetcode/src/main/java/com/example/leetcode/leetcode/LeetCode769.java
@@ -0,0 +1,15 @@
+package com.example.leetcode.leetcode;
+
+/**
+ * @author pano
+ * @date 2022/10/13
+ * @url <a href="https://leetcode.cn/problems/max-chunks-to-make-sorted/">max-chunks-to-make-sorted</a>
+ **/
+public class LeetCode769 {
+
+    public int maxChunksToSorted(int[] arr) {
+
+        
+        return 0;
+    }
+}
