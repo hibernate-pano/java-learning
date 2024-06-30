d95c423531ba39ec4f07bf3ce4973f293103f9a5
panbo <362472407@qq.com>
2024-06-30 22:39:28 +0800

    docs(synchronized): 补充 synchronized 的代码文档

--- java-mistakes/src/main/java/com/example/mistakes/demos/SynchronizedDemo2.java
+++ java-mistakes/src/main/java/com/example/mistakes/demos/SynchronizedDemo2.java
@@ -17,14 +17,19 @@ class SynchronizedDemo2 {
     public static void main(String[] args) {
         SynchronizedDemo2.reset();
         log.info("start -- counter=[{}]", SynchronizedDemo2.getCounter());
-        //多线程循环一定次数调用Data类不同实例的wrong方法
+        // 多线程循环一定次数调用Data类不同实例的wrong方法
         IntStream.rangeClosed(1, 1000000).parallel().forEach(i -> new SynchronizedDemo2().wrong());
+//        // 多线程循环一定次数调用System.out.println
+//        IntStream.rangeClosed(1, 1000).parallel().forEach(
+//                i -> System.out.println("Thread " + Thread.currentThread().getName() + " running " + i)
+//        );
         log.info("end   -- counter=[{}]", SynchronizedDemo2.getCounter());
     }
 
     public void wrong() {
         synchronized (SynchronizedDemo2.class) {
             counter++;
+            // 错误：调用了System.out.println，导致线程不安全
         }
     }
 }
