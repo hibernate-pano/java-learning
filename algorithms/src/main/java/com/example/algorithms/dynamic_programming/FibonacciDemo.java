ee195e1fd806b3096842f55777617f4c7243d173
panbo <panbo.coding@qq.com>
2025-08-12 14:27:54 +0800

    feat(algorithms): 优化斐波那契数列算法并添加新解法
    - 移除了迭代方法和递归方法中的打印语句
    - 优化了递归方法中的 memo 数组更新逻辑
    - 添加了空间复杂度为O(1) 的最优解法 fibonacci_optimized
    - 在主函数中添加了新解法的测试代码

--- algorithms/src/main/java/com/example/algorithms/dynamic_programming/FibonacciDemo.java
+++ algorithms/src/main/java/com/example/algorithms/dynamic_programming/FibonacciDemo.java
@@ -16,7 +16,6 @@ public class FibonacciDemo {
      * @return 第 n 个斐波那契数的值
      */
     public static int fibonacci(int n) {
-        System.out.println("启动迭代方法...");
         // 1. 定义状态：dp[i] 表示第 i 个斐波那契数的值
         // 新建一个长度为 n+1 的数组，用来保存从 0 到 n 的所有斐波那契数；
         int[] dp = new int[n + 1];
@@ -35,7 +34,6 @@ public class FibonacciDemo {
 
 
     public static int fibonacci_with_memo(int n, int[] memo) {
-        System.out.println("开始递归方法...");
         // 使用 memo 记忆功能（实际上就是缓存）来做斐波那契数
         // 一般使用数组或者哈希表来做 memo
         // 判断长度，如果溢出，返回 -1
@@ -50,7 +48,30 @@ public class FibonacciDemo {
         if (memo[n] > 0) {
             return memo[n];
         }
-        return fibonacci_with_memo(n - 1, memo) + fibonacci_with_memo(n - 2, memo);
+        memo[n] = fibonacci_with_memo(n - 1, memo) + fibonacci_with_memo(n - 2, memo);
+
+        return memo[n];
+    }
+
+    /**
+     * 这个优化版本的空间复杂度仅为O(1)，是计算斐波那契数列的最优解法。
+     */
+    public static int fibonacci_optimized(int n) {
+        if (n < 2) {
+            return n;
+        }
+
+        int prev2 = 0; // F(0)
+        int prev1 = 1; // F(1)
+        int current = 0;
+
+        for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
     }
 
 
@@ -63,5 +84,8 @@ public class FibonacciDemo {
         int[] memo = new int[n + 1];
         int result2 = fibonacci_with_memo(n, memo);
         System.out.println("斐波那契数列2的第 " + n + " 项是: " + result2); //
+
+        int result3 = fibonacci_optimized(n);
+        System.out.println("斐波那契数列3的第 " + n + " 项是: " + result3); //
     }
 }
