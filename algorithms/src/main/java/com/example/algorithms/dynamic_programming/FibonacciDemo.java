cae554c9307fe7fdccf001fca8d61dbfe0ce55b2
panbo <panbo.coding@qq.com>
2025-08-12 14:19:49 +0800

    feat(algorithms): 添加斐波那契数列的递归实现并优化性能
    - 新增带记忆功能的递归方法 fibonacci_with_memo
    - 优化了原有迭代方法的输出信息
    - 在 main 函数中添加了两种方法的对比示例

--- algorithms/src/main/java/com/example/algorithms/dynamic_programming/FibonacciDemo.java
+++ algorithms/src/main/java/com/example/algorithms/dynamic_programming/FibonacciDemo.java
@@ -16,6 +16,7 @@ public class FibonacciDemo {
      * @return 第 n 个斐波那契数的值
      */
     public static int fibonacci(int n) {
+        System.out.println("启动迭代方法...");
         // 1. 定义状态：dp[i] 表示第 i 个斐波那契数的值
         // 新建一个长度为 n+1 的数组，用来保存从 0 到 n 的所有斐波那契数；
         int[] dp = new int[n + 1];
@@ -32,10 +33,35 @@ public class FibonacciDemo {
         return dp[n];
     }
 
+
+    public static int fibonacci_with_memo(int n, int[] memo) {
+        System.out.println("开始递归方法...");
+        // 使用 memo 记忆功能（实际上就是缓存）来做斐波那契数
+        // 一般使用数组或者哈希表来做 memo
+        // 判断长度，如果溢出，返回 -1
+        if (memo.length < n + 1) {
+            return -1;
+        }
+        // 0 = 0 ,1 =1
+        if (n < 2) {
+            return n;
+        }
+        // 如果有数据，则获取
+        if (memo[n] > 0) {
+            return memo[n];
+        }
+        return fibonacci_with_memo(n - 1, memo) + fibonacci_with_memo(n - 2, memo);
+    }
+
+
     public static void main(String[] args) {
         // 示例
         int n = 10;
         int result = fibonacci(n);
-        System.out.println("斐波那契数列的第 " + n + " 项是: " + result); // 输出: 斐波那契数列的第 10 项是: 55
+        System.out.println("斐波那契数列1的第 " + n + " 项是: " + result); // 输出: 斐波那契数列的第 10 项是: 55
+
+        int[] memo = new int[n + 1];
+        int result2 = fibonacci_with_memo(n, memo);
+        System.out.println("斐波那契数列2的第 " + n + " 项是: " + result2); //
     }
 }
