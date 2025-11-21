687145fe2f6e576b1d5d4db8e73370c796d5c459
panbo <panbo.coding@qq.com>
2025-11-21 11:27:59 +0800

    feat(算法): 实现插入排序算法并附示例
    - 为整数数组添加了 insertionSort 方法以实现排序
    - 在主方法中包含一个示例数组用于演示
    - 实现了通过关键元素插入和数据移动的排序逻辑
    - 添加了控制台输出，以展示排序前后的状态

--- algorithms/src/main/java/com/example/algorithms/sorting/InsertionSort.java
+++ algorithms/src/main/java/com/example/algorithms/sorting/InsertionSort.java
@@ -10,4 +10,40 @@ package com.example.algorithms.sorting;
  * @create_time 2025/11/21 10:28
  **/
 public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i]; // 当前要插入的元素
+            int j = i - 1; // 已排序部分的最后一个索引
+
+            // 将大于key的元素向后移动
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            // 插入key到正确位置
+            arr[j + 1] = key;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        System.out.println("排序前的数组：");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        // 调用插入排序方法
+        insertionSort(arr);
+
+        System.out.println();
+        System.out.println("排序后的数组：");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
+
+
+
